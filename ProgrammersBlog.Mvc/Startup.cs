46713da41c3a105691221692237d729f9adf5ca6
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-10-20 18:28:51 +0300

    [Feature]: ServiceCollectionExtensions ve LoadMyServices Metodu Oluşturma
    * Yeni sınıf oluşturma
      - ServiceCollectionExtensions sınıfı başarıyla oluşturuldu.
    * Yeni metot oluşturma
      - LoadMyServices metodu ServiceCollectionExtensions sınıfına eklenmiştir.
    * LoadMyServices içerik tanımlamaları
      - Metot içerisine DbContext tanımlaması yapıldı.
      - UnitOfWork tanımlaması eklenmiştir.
      - Manager tanımlamaları başarıyla eklendi.

--- ProgrammersBlog.Mvc/Startup.cs
+++ ProgrammersBlog.Mvc/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProgrammersBlog.Services.Extensions;
 
 namespace ProgrammersBlog.Mvc
 {
@@ -16,6 +17,7 @@ namespace ProgrammersBlog.Mvc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.LoadMyServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concrate;
+using ProgrammersBlog.Data.Concrate.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Services.Concrete;
+
+namespace ProgrammersBlog.Services.Extensions
+{
+  public static class ServiceCollectionExtensions
+  {
+    public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddDbContext<ProgrammersBlogContext>();
+      serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+      serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+      serviceCollection.AddScoped<IArticleService, ArticleManager>();
+      return serviceCollection;
+    }
+  }
+}
