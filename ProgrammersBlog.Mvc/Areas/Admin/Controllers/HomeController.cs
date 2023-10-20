60b59c0b044597ea22c4c7a893fd2f09a6a79f9c
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-10-20 18:56:06 +0300

    [Feature]: MVC Yapısı ve Admin Area Oluşturma
    * Temel MVC yapısı
      - Projede temel MVC yapısı başarıyla oluşturulmuştur.
    * Admin Area oluşturma
      - "Admin" adında bir area başarıyla eklenmiştir.
    * Temel View dosyaları
      - ViewImports, Layout ve ViewStart dosyaları oluşturulmuştur.
    * İlk Controller ve View oluşturma
      - HomeController başarıyla oluşturuldu.
      - HomeController için ilgili home view dosyası eklenmiştir.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class HomeController : Controller
+  {
+    // GET
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammersBlog.Mvc.HomeController
+{
+  public class HomeController : Controller
+  {
+    // GET
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
