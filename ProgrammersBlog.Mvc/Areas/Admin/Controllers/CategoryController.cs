0b19e272640911ec12ec676340e0ec8a064342ff
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-10-20 22:42:56 +0300

    [Feature]: Category View, Controller ve DataTable Ayarlamaları
    * Category View ve Controller
      - Category View ve Controller oluşturuldu.
      - ICategoryService tanımı gerçekleştirilip constructor'a eklenmiştir.
    * DataTable özelliği
      - DataTable özelliği başarıyla oluşturuldu.
      - "Yenile" ve "Ekle" isimli 2 yeni özel buton DataTable'a eklenmiştir.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilites.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class CategoryController : Controller
+  {
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+      _categoryService = categoryService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      var result = await _categoryService.GetAll();
+      if (result.ResultStatus == ResultStatus.Success)
+      {
+        return View(result.Data);
+      }
+      return View();
+    }
+  }
+}
