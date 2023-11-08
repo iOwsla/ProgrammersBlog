904587da463d7d17a32f844a040835cff33ee67b
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-11-08 10:37:27 +0300

    [Feature]: EntityRepositoryBase ve CategoryService Güncellemeleri
    * EntityRepositoryBase ve EFRepositoryBase güncellemeleri
      - Add ve Update metotları artık entity türünde bir değer döndürüyor.
    * ICategoryService ve CategoryService güncellemeleri
      - EntityRepositoryBase'deki güncellemeler ICategoryService ve CategoryService sınıflarına uygulandı.
      - Bu sayede, veri ekleme ve güncelleme işlemlerinde dönen veriler, frontend tarafında işlenebilecek.

--- ProgrammersBlog.Services/Concrete/CategoryManager.cs
+++ ProgrammersBlog.Services/Concrete/CategoryManager.cs
@@ -38,7 +38,12 @@ namespace ProgrammersBlog.Services.Concrete
         });
       }
 
-      return new DataResult<CategoryDto>(ResultStatus.Error, "Böyle bir kategori bulunamadı.", null);
+      return new DataResult<CategoryDto>(ResultStatus.Error, "Böyle bir kategori bulunamadı.", new CategoryDto
+      {
+          Category = null,
+          ResultStatus = ResultStatus.Error,
+          Message = "Böyle bir kategori bulunamadı."
+      });
     }
 
     public async Task<IDataResult<CategoryListDto>> GetAll()
@@ -107,7 +112,7 @@ namespace ProgrammersBlog.Services.Concrete
       });
     }
 
-    public async Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName)
+    public async Task<IDataResult<CategoryDto>> Add(CategoryAddDto categoryAddDto, string createdByName)
     {
       var category = _mapper.Map<Category>(categoryAddDto);
 
@@ -115,24 +120,34 @@ namespace ProgrammersBlog.Services.Concrete
 
       category.ModifiedByName = createdByName;
   
-      await _unitOfWork.Categories.AddAsync(category);
+      var addedCategory = await _unitOfWork.Categories.AddAsync(category);
       
       await _unitOfWork.SaveAsync();
 
-      return new Result(ResultStatus.Success, $"{categoryAddDto.Name} + adlı kategori başarıyla eklenmiştir.");
+      return new DataResult<CategoryDto>(ResultStatus.Success, $"{categoryAddDto.Name} + adlı kategori başarıyla eklenmiştir.", new CategoryDto
+      {
+          Category = addedCategory,
+          ResultStatus = ResultStatus.Success,
+          Message = $"{categoryAddDto.Name} + adlı kategori başarıyla eklenmiştir."
+      });
     }
 
-    public async Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName)
+    public async Task<IDataResult<CategoryDto>> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName)
     {
       var category = _mapper.Map<Category>(categoryUpdateDto);
 
       category.ModifiedByName = modifiedByName;
 
-      await _unitOfWork.Categories.UpdateAsync(category);
+      var updatedCategory = await _unitOfWork.Categories.UpdateAsync(category);
         
       await _unitOfWork.SaveAsync();
       
-      return new Result(ResultStatus.Success, $"{categoryUpdateDto.Name} + adlı kategori başarıyla güncellenmiştir.");
+      return new DataResult<CategoryDto>(ResultStatus.Success, $"{categoryUpdateDto.Name} + adlı kategori başarıyla güncellenmiştir.", new CategoryDto
+      {
+          Category = updatedCategory,
+          ResultStatus = ResultStatus.Success,
+          Message = $"{categoryUpdateDto.Name} + adlı kategori başarıyla güncellenmiştir."
+      });
     }
 
     public async Task<IResult> Delete(int categoryId, string modifiedByName)
--- ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -19,9 +19,10 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
             _context = context;
         }
 
-        public async Task AddAsync(TEntity entity)
+        public async Task<TEntity> AddAsync(TEntity entity)
         {
             await _context.Set<TEntity>().AddAsync(entity);
+            return entity;
         }
 
         public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
@@ -73,9 +74,10 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
             return await query.SingleOrDefaultAsync();
         }
 
-        public async Task UpdateAsync(TEntity entity)
+        public async Task<TEntity> UpdateAsync(TEntity entity)
         {
             await Task.Run(() => { _context.Set<TEntity>().Update(entity); }); // eğer metotta async yoksa kendimiz Task.Run() ile async işlemi yapıyoruz.
+            return entity;
         }
     }
 }
